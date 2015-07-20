6e531400cf02dfeaf1b0546a6629329e9a8f6def
Nithya Pari <nithya1308@gmail.com>
2015-07-20 14:53:51 -0700

    Given an array of distinct integer, count the number of pairs of integers that have difference k.

---
+++ src/tip1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+/**
+ * Created by nithyapari on 7/20/15.
+ */
+public class tip1 {
+
+
+    public int countPairsDiffTwo(){
+
+        int[] numArray = {1, 3, 5, 7};
+        int diff = 2;
+        int pairCount = 0;
+
+//Create a HashMap from Array
+        HashMap<Integer, Boolean> numbers = new HashMap<Integer, Boolean>();
+        for(int num : numArray){
+            numbers.put(num, true);
+        }
+
+//Checking if num + diff or num - diff is present in the HashMap
+        for(int num : numArray){
+            if(numbers.containsKey(num + diff) || numbers.containsKey(num - diff)) {
+
+                pairCount++;
+            }
+        }
+
+        return pairCount;
+
+    }
+
+    public static void main(String[] args) {
+        tip1 obj = new tip1();
+        System.out.println("Number of Pairs of Diff 2: "+obj.countPairsDiffTwo());
+    }
+}
