f333ff6dfb4f5f0bf8c1643b41d012179b2475a0
Nithya Pari <nithya1308@gmail.com>
2015-08-20 23:43:28 -0700

    Recursion - Factorial Permutation

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -15,13 +15,15 @@ public class Factorial {
     *
     */
 
-    /*
-    *  Test Cases - Throw error messages
-    *  --------------------------------
-    *  If number is empty
-    *  If number is negative
-    *
-    *
+    /* Pseudocode:
+     *   Check if input number n is 1 or 0 and if yes, return 1 // 0! = 1! = 1
+     *  Else recursively call the same function with value (n-1) and multiply the return value by n
+
+     * Time Complexity:
+       O(n)
+
+     * Space Complexity:
+       O(1)
     */
 
     int factorial(int inputNumber){
--- src/Recursion/Permutation.java
+++ src/Recursion/Permutation.java
@@ -6,13 +6,51 @@ package Recursion;
  */
 public class Permutation {
 
-    StringBuilder output = new StringBuilder();
-    Boolean[] used;
+    static String input = "abc";
+    static int length = input.length();
 
+    static StringBuilder output = new StringBuilder();
+    static boolean[] usedLetter = new boolean[length];
 
+    static int j = 0;
+
+    public static void main(String[] args) {
+
+        Permutation obj = new Permutation();
+        obj.permute();
+
+    }
 
     //Recursive Function
-    void permute(){
+    void permute() {
+
+        if (output.length() == length) {
+            System.out.println(output.toString());
+            return;
+        }
+
+        for (int i = 0; i < length; i++) {
+
+            //Check if Start Letter has been used already, then skip
+            if (usedLetter[i])
+                continue;
+            else {
+                //Set it as Start Letter
+                usedLetter[i] = true;
+
+                //Append to String Builder
+                output.append(input.charAt(i));
+
+                //Recursion Call - This will complete all permutations with Start Letter that was chosen
+                permute();
+
+                //After printing the permutation, return back and set the last character as not used now
+                usedLetter[i] = false;
+
+            }
+
+        }
+
 
     }
 
